a5c2fdef1f2773a51299c432a73323d0b6b14394
Maxim Egorov <13maximegorov@gmail.com>
2021-02-16 14:43:07 +0500

    update file index.js

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
 function mobileRemote(text) {
+  const keyboard = [
+    ['1'],
+    ['2', 'a', 'b', 'c'],
+    ['3', 'd', 'e', 'f'],
+    ['4', 'g', 'h', 'i'],
+    ['5', 'j', 'k', 'l'],
+    ['6', 'm', 'n', 'o'],
+    ['7', 'p', 'q', 'r', 's'],
+    ['8', 't', 'u', 'v'],
+    ['9', 'w', 'x', 'y', 'z'],
+  ]
+
+  let currentCoords = [0, 0]
+  let register = 0 // Caps Lock
+  let textArray = text.split('')
+  let steps = 0
 
 }
 
